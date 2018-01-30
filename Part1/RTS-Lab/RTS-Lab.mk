30a9c51efe34f59dfeb09ed89bd15b2b7b66307a
Fredrik Johansson <frejohi@net.chalmers.se>
2018-01-30 12:01:46 +0100

    Part 1 done

--- Part1/RTS-Lab/RTS-Lab.mk
+++ Part1/RTS-Lab/RTS-Lab.mk
@@ -5,15 +5,15 @@
 ## Debug
 ProjectName            :=RTS-Lab
 ConfigurationName      :=Debug
-WorkspacePath          :=Z:/Documents/TinyTimber/RTSRTS/Part1
-ProjectPath            :=Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab
+WorkspacePath          :=Z:/RTS/RTSRTS/Part1
+ProjectPath            :=Z:/RTS/RTSRTS/Part1/RTS-Lab
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=holdo
-Date                   :=23/01/2018
+User                   :=frejohi
+Date                   :=30/01/2018
 CodeLitePath           :=C:/cseapp/CodeLite
 LinkerName             :=$(CodeLiteDir)/tools/gcc-arm/bin/arm-none-eabi-g++.exe
 SharedObjectLinkerName :=$(CodeLiteDir)/tools/gcc-arm/arm-none-eabi-g++.exe -shared -fPIC
@@ -100,7 +100,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/application.c$(ObjectSuffix): application.c $(IntermediateDirectory)/application.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/application.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/application.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/application.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/application.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/application.c$(DependSuffix): application.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/application.c$(ObjectSuffix) -MF$(IntermediateDirectory)/application.c$(DependSuffix) -MM application.c
 
@@ -108,7 +108,7 @@ $(IntermediateDirectory)/application.c$(PreprocessSuffix): application.c
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/application.c$(PreprocessSuffix) application.c
 
 $(IntermediateDirectory)/sciTinyTimber.c$(ObjectSuffix): sciTinyTimber.c $(IntermediateDirectory)/sciTinyTimber.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/sciTinyTimber.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/sciTinyTimber.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/sciTinyTimber.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/sciTinyTimber.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/sciTinyTimber.c$(DependSuffix): sciTinyTimber.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/sciTinyTimber.c$(ObjectSuffix) -MF$(IntermediateDirectory)/sciTinyTimber.c$(DependSuffix) -MM sciTinyTimber.c
 
@@ -116,7 +116,7 @@ $(IntermediateDirectory)/sciTinyTimber.c$(PreprocessSuffix): sciTinyTimber.c
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/sciTinyTimber.c$(PreprocessSuffix) sciTinyTimber.c
 
 $(IntermediateDirectory)/startup.c$(ObjectSuffix): startup.c $(IntermediateDirectory)/startup.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/startup.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/startup.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/startup.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/startup.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/startup.c$(DependSuffix): startup.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/startup.c$(ObjectSuffix) -MF$(IntermediateDirectory)/startup.c$(DependSuffix) -MM startup.c
 
@@ -124,7 +124,7 @@ $(IntermediateDirectory)/startup.c$(PreprocessSuffix): startup.c
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/startup.c$(PreprocessSuffix) startup.c
 
 $(IntermediateDirectory)/TinyTimber.c$(ObjectSuffix): TinyTimber.c $(IntermediateDirectory)/TinyTimber.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/TinyTimber.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/TinyTimber.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/TinyTimber.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/TinyTimber.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/TinyTimber.c$(DependSuffix): TinyTimber.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/TinyTimber.c$(ObjectSuffix) -MF$(IntermediateDirectory)/TinyTimber.c$(DependSuffix) -MM TinyTimber.c
 
@@ -132,7 +132,7 @@ $(IntermediateDirectory)/TinyTimber.c$(PreprocessSuffix): TinyTimber.c
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/TinyTimber.c$(PreprocessSuffix) TinyTimber.c
 
 $(IntermediateDirectory)/dispatch.s$(ObjectSuffix): dispatch.s $(IntermediateDirectory)/dispatch.s$(DependSuffix)
-	$(AS) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/dispatch.s" $(ASFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/dispatch.s$(ObjectSuffix) -I$(IncludePath)
+	$(AS) "Z:/RTS/RTSRTS/Part1/RTS-Lab/dispatch.s" $(ASFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/dispatch.s$(ObjectSuffix) -I$(IncludePath)
 $(IntermediateDirectory)/dispatch.s$(DependSuffix): dispatch.s
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dispatch.s$(ObjectSuffix) -MF$(IntermediateDirectory)/dispatch.s$(DependSuffix) -MM dispatch.s
 
@@ -140,7 +140,7 @@ $(IntermediateDirectory)/dispatch.s$(PreprocessSuffix): dispatch.s
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dispatch.s$(PreprocessSuffix) dispatch.s
 
 $(IntermediateDirectory)/canTinyTimber.c$(ObjectSuffix): canTinyTimber.c $(IntermediateDirectory)/canTinyTimber.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/canTinyTimber.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/canTinyTimber.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/canTinyTimber.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/canTinyTimber.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/canTinyTimber.c$(DependSuffix): canTinyTimber.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/canTinyTimber.c$(ObjectSuffix) -MF$(IntermediateDirectory)/canTinyTimber.c$(DependSuffix) -MM canTinyTimber.c
 
@@ -148,7 +148,7 @@ $(IntermediateDirectory)/canTinyTimber.c$(PreprocessSuffix): canTinyTimber.c
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/canTinyTimber.c$(PreprocessSuffix) canTinyTimber.c
 
 $(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(ObjectSuffix): driver/src/stm32f4xx_dac.c $(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_dac.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_dac.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(DependSuffix): driver/src/stm32f4xx_dac.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(ObjectSuffix) -MF$(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(DependSuffix) -MM driver/src/stm32f4xx_dac.c
 
@@ -156,7 +156,7 @@ $(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(PreprocessSuffix): driver/s
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/driver_src_stm32f4xx_dac.c$(PreprocessSuffix) driver/src/stm32f4xx_dac.c
 
 $(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(ObjectSuffix): driver/src/stm32f4xx_gpio.c $(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_gpio.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_gpio.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(DependSuffix): driver/src/stm32f4xx_gpio.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(ObjectSuffix) -MF$(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(DependSuffix) -MM driver/src/stm32f4xx_gpio.c
 
@@ -164,7 +164,7 @@ $(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(PreprocessSuffix): driver/
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/driver_src_stm32f4xx_gpio.c$(PreprocessSuffix) driver/src/stm32f4xx_gpio.c
 
 $(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(ObjectSuffix): driver/src/stm32f4xx_rcc.c $(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_rcc.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_rcc.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(DependSuffix): driver/src/stm32f4xx_rcc.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(ObjectSuffix) -MF$(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(DependSuffix) -MM driver/src/stm32f4xx_rcc.c
 
@@ -172,7 +172,7 @@ $(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(PreprocessSuffix): driver/s
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/driver_src_stm32f4xx_rcc.c$(PreprocessSuffix) driver/src/stm32f4xx_rcc.c
 
 $(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(ObjectSuffix): driver/src/stm32f4xx_tim.c $(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_tim.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_tim.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(DependSuffix): driver/src/stm32f4xx_tim.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(ObjectSuffix) -MF$(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(DependSuffix) -MM driver/src/stm32f4xx_tim.c
 
@@ -180,7 +180,7 @@ $(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(PreprocessSuffix): driver/s
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/driver_src_stm32f4xx_tim.c$(PreprocessSuffix) driver/src/stm32f4xx_tim.c
 
 $(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(ObjectSuffix): driver/src/stm32f4xx_usart.c $(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_usart.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_usart.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(DependSuffix): driver/src/stm32f4xx_usart.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(ObjectSuffix) -MF$(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(DependSuffix) -MM driver/src/stm32f4xx_usart.c
 
@@ -188,7 +188,7 @@ $(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(PreprocessSuffix): driver
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/driver_src_stm32f4xx_usart.c$(PreprocessSuffix) driver/src/stm32f4xx_usart.c
 
 $(IntermediateDirectory)/driver_src_stm32f4xx_can.c$(ObjectSuffix): driver/src/stm32f4xx_can.c $(IntermediateDirectory)/driver_src_stm32f4xx_can.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "Z:/Documents/TinyTimber/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_can.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_can.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "Z:/RTS/RTSRTS/Part1/RTS-Lab/driver/src/stm32f4xx_can.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/driver_src_stm32f4xx_can.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/driver_src_stm32f4xx_can.c$(DependSuffix): driver/src/stm32f4xx_can.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/driver_src_stm32f4xx_can.c$(ObjectSuffix) -MF$(IntermediateDirectory)/driver_src_stm32f4xx_can.c$(DependSuffix) -MM driver/src/stm32f4xx_can.c
 
--- Part1/RTS-Lab/application.c
+++ Part1/RTS-Lab/application.c
@@ -8,20 +8,32 @@ typedef struct {
     Object super;
     int count;
     char c;
+	int tone;
 	int volume;
+	int muteFlag;
+	int deadlineFlag;
 	char buf[1000];
 } App;
+
+typedef struct {
+	Object super;
+	int background_loop_range;
+	int deadlineFlag;
+} Background;
 
 int bJohn[32] = {0,2,4,0,0,2,4,0,4,5,7,4,5,7,7,9,7,5,4,0,7,9,7,5,4,0,0,-5,0,0,-5,0};
 int key[11] = {-5,-4,-3,-2,-1,0,1,2,3,4,5};
 int period[25] = {2025,1911,1804,1703,1607,1517,1432,1351,1276,1204,1136,1073,1012,956,902,851,804,758,716,676,638,602,568,536,506};
+int test[3] = {500,650,931};
 
-App app = { initObject(), 0, 'X' };
+App app = { initObject(), 0, 'X', 0,0,0,1 };
+Background background = { initObject(), 1000,1 };
 
 void reader(App*, int);
 void receiver(App*, int);
 
-#define DAC_REGISTER (*(volatile uint8_t*)0x4000741C)
+#define DAC_REGISTER (*(char *)0x4000741C)
+
 Serial sci0 = initSerial(SCI_PORT0, &app, reader);
 
 Can can0 = initCan(CAN_PORT0, &app, receiver);
@@ -34,6 +46,78 @@ void printPeriods(int k){
 	}
 }
 
+void checkToneTime(App *self){
+	for(int i = 0; i < 1000; i++){
+		DAC_REGISTER = self->volume;
+	}
+}
+
+void checkToneBack(Background *self){
+	for(int i = 0; i < 13500; i++){
+		
+	}
+}
+
+void Controller(App *self, int arg){
+	Time Start, Diff, sum;
+	Time m = 0;
+	Time times[1000];
+	int a;
+	
+	for(int i = 0; i < 500; i++) {
+		Start = CURRENT_OFFSET();
+		checkToneBack(&background);
+		Diff = CURRENT_OFFSET() - Start;
+		times[i] = Diff;
+	}
+	for(int j = 0; j < 500; j++){
+		sum += times[j];
+		if(times[j] > m)
+			m = times[j];
+	}
+	a = USEC_OF(sum)/500;
+	char str[1000];
+	sprintf(str, "Sum: %ld \nMax: %ld \nAvg: %d",USEC_OF(sum), USEC_OF(m), a );
+	SCI_WRITE(&sci0, str);
+}
+
+void backgoundLoopTask(Background *self, int arg) {
+	for(int i = 0; i < self->background_loop_range; i++){
+		
+	}
+	if(self->deadlineFlag)
+		SEND(USEC(1300),USEC(1300),self,backgoundLoopTask, 0);
+	else
+		AFTER(USEC(1300),self,backgoundLoopTask, 0);
+		
+}
+
+void writeOneToDAC(App *self, int t);
+
+void writeZeroToDAC(App *self, int t) {
+	DAC_REGISTER = 0x0;
+	if(self->deadlineFlag)
+		SEND(USEC(test[self->tone]),USEC(100), self, writeOneToDAC, t);
+	else
+		AFTER(USEC(test[self->tone]), self, writeOneToDAC, t);}
+
+void writeOneToDAC(App *self, int t) {
+	if(!self->muteFlag){
+		DAC_REGISTER = self->volume;
+	}else{
+		DAC_REGISTER = 0x0;
+	}
+	if(self->deadlineFlag)
+		SEND(USEC(test[self->tone]),USEC(100), self, writeZeroToDAC, t);
+	else
+		AFTER(USEC(test[self->tone]), self, writeZeroToDAC, t);
+
+}
+
+void toneGenerator(App *self, int t) {
+	writeOneToDAC(self, t);
+}
+
 void receiver(App *self, int unused) {
     CANMsg msg;
     CAN_RECEIVE(&can0, &msg);
@@ -41,49 +125,56 @@ void receiver(App *self, int unused) {
     SCI_WRITE(&sci0, msg.buff);
 }
 
+void mute(App *self) {
+	if(!self->muteFlag){
+		self->muteFlag = 1;
+	}else {
+		self->muteFlag = 0;
+	}
+}
+
+void changeLoad(Background *self, int c){
+	if(c == 'u'){
+		self->background_loop_range += 500;			
+	}
+	else{
+		self->background_loop_range -= 500;
+	}
+	char str[1000];
+	sprintf(str, "%d \n", self->background_loop_range);
+	SCI_WRITE(&sci0, str);
+} 
+void toggleDeadline(Background *self){
+	if(self->deadlineFlag == 0){
+		self->deadlineFlag = 1;
+		self->deadlineFlag = 1;
+	}else{
+		self->deadlineFlag = 0;
+		self->deadlineFlag = 0;
+	}
+}
+
 void reader(App *self, int c) {
-	
-	int num;
-	
+		
 	SCI_WRITE(&sci0, "Rcv: \'");
 	SCI_WRITECHAR(&sci0, c);
 	SCI_WRITE(&sci0, "\'\n");
 	if(c == 'm'){
-		
-	}	
-	if(c == 'e' ){
-		self->buf[self->count] = '\0';
-		num = atoi(self->buf);
-		self->count = 0;
-		if(num >= 1 && num <= 20){
-		}
+		mute(self);
+	}else if(c == '+') {
+		if(self->volume < 20)
+			self->volume++;
+	}else if(c == '-') {
+		if(self->volume > 1 )
+			self->volume--;
+	}else if(c == 'u' || c == 'j') {
+		changeLoad(&background, c);
+	}else if(c == 'd') {
+		toggleDeadline(&background);
+	}else if(c >= '1' && c <='3') {
+		self->tone = c-'1';
 	}
-	else if(c == '-' || (c >= '0' && c <= '9')){
-		self->buf[self->count++] = c;
-	}
-	/*int num;
-	char str[1000];
 	
-	SCI_WRITE(&sci0, "Rcv: \'");
-	SCI_WRITECHAR(&sci0, c);
-	SCI_WRITE(&sci0, "\'\n");
-		
-	if(c == 'e'){
-		self->buf[self->count] = '\0';
-		num = atoi(self->buf);
-		self->myNum = num + self->myNum;
-		sprintf(str, "The entered number is %d \nThe running sum is %d \n", num, self->myNum);
-		SCI_WRITE(&sci0, str);
-		self->count = 0;
-	}
-	else if(c == 'F'){
-		self->myNum = 0;
-		sprintf(str, "The running sum is %d \n", self->myNum);
-		SCI_WRITE(&sci0, str);
-	}
-	else if(c == '-' || (c >= '0' && c <= '9')){
-		self->buf[self->count++] = c;
-	}*/
 }
 
 void startApp(App *self, int arg) {
@@ -103,7 +194,10 @@ void startApp(App *self, int arg) {
     msg.buff[4] = 'o';
     msg.buff[5] = 0;
     CAN_SEND(&can0, &msg);
-	printPeriods(0);
+	self->volume = 0x5;
+	Controller(self,0);
+	//ASYNC(self, toneGenerator, 0);
+	//ASYNC(&background, backgoundLoopTask, 0);
 }
 
 int main() {
